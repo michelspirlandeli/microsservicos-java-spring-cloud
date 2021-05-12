e7743f7ca74cc909b31f61fd244d3d7f72086378
michelspirlandeli <michelspirlandeli10@gmail.com>
2021-05-12 20:01:37 -0300

    Feign

--- hr-payroll/src/main/java/com/michelspirlandeli/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/michelspirlandeli/hrpayroll/services/PaymentService.java
@@ -1,13 +1,27 @@
 package com.michelspirlandeli.hrpayroll.services;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 import com.michelspirlandeli.hrpayroll.entities.Payment;
+import com.michelspirlandeli.hrpayroll.entities.Worker;
+import com.michelspirlandeli.hrpayroll.feignclients.WorkerFeignClient;
 
 @Service
 public class PaymentService {
 
+	@Autowired
+	private WorkerFeignClient workerFeignClient;
+
 	public Payment getPayment(long workerId, int days) {
-				
-		return new Payment("Bob", 200.0, days);
+
+		Map<String, String> uriVariables = new HashMap<>();
+		uriVariables.put("id", ""+workerId);
+
+		Worker worker = workerFeignClient.findById(workerId).getBody();
+		return new Payment(worker.getName(), worker.getDailyIncome(), days);
 	}
 }
