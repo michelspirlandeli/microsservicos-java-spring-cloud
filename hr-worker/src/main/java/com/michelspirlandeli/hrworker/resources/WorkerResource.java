c23283ca543b84a6bdc23cdc8f3973c5933517e3
michelspirlandeli <michelspirlandeli10@gmail.com>
2021-05-15 13:27:25 -0300

    Ribbon load balancing

--- hr-worker/src/main/java/com/michelspirlandeli/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/michelspirlandeli/hrworker/resources/WorkerResource.java
@@ -2,7 +2,11 @@ package com.michelspirlandeli.hrworker.resources;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,6 +20,11 @@ import com.michelspirlandeli.hrworker.repositories.WorkerRepository;
 @RequestMapping(value = "/workes")
 public class WorkerResource {
 
+	private static Logger logger = LoggerFactory.getLogger(WorkerResource.class);
+
+	@Autowired
+	private Environment env;
+	
 	@Autowired
 	private WorkerRepository workerRepository;
 	
@@ -27,6 +36,9 @@ public class WorkerResource {
 	
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<Worker> findById(@PathVariable Long id){
+
+		logger.info("PORT = " + env.getProperty("local.server.port"));
+
 		Worker obj = workerRepository.findById(id).get();
 		return ResponseEntity.ok(obj);
 	}
