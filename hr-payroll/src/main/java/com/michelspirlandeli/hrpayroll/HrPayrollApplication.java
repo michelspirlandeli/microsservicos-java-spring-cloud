7fd575e9fad99c61d1dd5788d261137b0dd11924
michelspirlandeli <michelspirlandeli10@gmail.com>
2022-01-31 10:58:23 -0300

    randow port to hr-work

--- hr-payroll/src/main/java/com/michelspirlandeli/hrpayroll/HrPayrollApplication.java
+++ hr-payroll/src/main/java/com/michelspirlandeli/hrpayroll/HrPayrollApplication.java
@@ -2,10 +2,9 @@ package com.michelspirlandeli.hrpayroll;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@EnableEurekaClient
+//@EnableEurekaClient
 @EnableFeignClients
 @SpringBootApplication
 public class HrPayrollApplication {
