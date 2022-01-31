24ba0045804e0747ba44af2d53953009a1f1f83a
michelspirlandeli <michelspirlandeli10@gmail.com>
2022-01-31 11:17:15 -0300

    Hystrix fault tolerance

--- hr-payroll/src/main/java/com/michelspirlandeli/hrpayroll/HrPayrollApplication.java
+++ hr-payroll/src/main/java/com/michelspirlandeli/hrpayroll/HrPayrollApplication.java
@@ -2,9 +2,12 @@ package com.michelspirlandeli.hrpayroll;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-//@EnableEurekaClient
+@EnableCircuitBreaker
+@EnableEurekaClient
 @EnableFeignClients
 @SpringBootApplication
 public class HrPayrollApplication {
--- hr-payroll/src/main/java/com/michelspirlandeli/hrpayroll/resources/PaymentResource.java
+++ hr-payroll/src/main/java/com/michelspirlandeli/hrpayroll/resources/PaymentResource.java
@@ -1,5 +1,8 @@
 package com.michelspirlandeli.hrpayroll.resources;
 
+import com.michelspirlandeli.hrpayroll.entities.Payment;
+import com.michelspirlandeli.hrpayroll.services.PaymentService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,20 +10,23 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.michelspirlandeli.hrpayroll.entities.Payment;
-import com.michelspirlandeli.hrpayroll.services.PaymentService;
-
 @RestController
 @RequestMapping(value = "/payments")
 public class PaymentResource {
 
 	@Autowired
 	private PaymentService service;
-	
+
+	@HystrixCommand(fallbackMethod = "getPaymentAlternative")
 	@GetMapping(value = "/{workerId}/days/{days}")
 	public ResponseEntity<Payment> getPayment(@PathVariable Long workerId, @PathVariable Integer days) {
 		Payment payment = service.getPayment(workerId, days);
 		return ResponseEntity.ok(payment);
-	}	
+	}
+
+	public ResponseEntity<Payment> getPaymentAlternative(Long workerId, Integer days) {
+		Payment payment = new Payment("Brann", 400.0, days);
+		return ResponseEntity.ok(payment);
+	}
 	
 }
